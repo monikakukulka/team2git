eaa99f105ec899bc867d71772a9c9f71aa25c593
Zbych27 <Zbych27@poczta.onet.pl>
2022-10-30 15:16:59 +0100

    dodanie obsługi strefy

--- apka.py
+++ apka.py
@@ -1,7 +1,18 @@
+def NrStrefy():
+  strefa = input("podaj region: (EUR , USA, Other: ")
+  if strefa != "EUR" and strefa != "USA":
+    strefa = ""
+  return strefa
+
 wiek = input("Podaj wiek: ")
 # Sprawdzamy, czy wiek jest złożony z cyfr
 if wiek.isdigit() == False:
-    exit("Wiek musi być liczbą")
+  exit("Wiek musi być liczbą")
 wiek = int(wiek)
-if wiek >= 18
-    print("Witamy!")
+if wiek >= 18:
+  print("Witamy!")
+
+strefa = NrStrefy()
+print("wskazano", strefa)
+
+
