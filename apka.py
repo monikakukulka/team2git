bb5de4ee078db6fe7596f5514b5a232fa4576755
monikakukulka <monika.kukulka@gmail.com>
2022-10-30 15:12:31 +0100

    update apka.py

--- apka.py
+++ apka.py
@@ -3,5 +3,20 @@ wiek = input("Podaj wiek: ")
 if wiek.isdigit() == False:
     exit("Wiek musi być liczbą")
 wiek = int(wiek)
-if wiek >= 18
+if wiek >= 18:
     print("Witamy!")
+
+
+def plec(x):
+    if x == 1:
+        print("Aperol gratis")
+    else:
+        print("Pij i płać")
+
+
+x = int(input("Podaj płeć: kobieta(1), mężczyzna(2), inne(3) "))
+
+plec(x)
+
+
+
