2c5304e6fd571dc0129fd02b0ae2504d48a8de52
Mariuschm <mariusz.midzio@gamil.com>
2022-10-30 14:46:34 +0100

    Drobny refaktor

--- apka.py
+++ apka.py
@@ -7,18 +7,16 @@ wiek = int(wiek)
 #TODO:
 # specjalne ceny dla osob +40
 # przemyslenie klas rownowaznosci
-
-
 #KR 1 <=0
 #KR 2 1-18
 #KR 3 18-99
+#Tutaj obsługa kraju
 pelnoletni = (wiek >= 18 and wiek <40)
 pozaZakresem = (wiek <=0 or wiek>99)
 ponad40 = (wiek>=40 and wiek<=99)
 
 if pozaZakresem:
     exit(f"Podany wiek {wiek} nie mieście się w zakresie 1-99")
-#Tutaj obsługa kraju
 elif pelnoletni:
     print("Witamy!")
 elif ponad40: #specjalne ceny dla osob +40
